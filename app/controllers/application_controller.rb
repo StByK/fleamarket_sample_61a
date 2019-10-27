e9f260651f5671a240ba3153d7a389e259df375b
StByK <miz07og18kag@gmail.com>
2019-10-27 12:13:01 +0900

    set devise strong-parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [
+        :nickname, :birthday, :first_bame, :last_name, :first_name_kana,
+        :last_name_kana, :phone_number, :profile_text, :icon_image,
+      ])
+  end
 end
