9d9551bd3647f02d2d0b723e3c5608e5c63f2ea5
StByK <miz07og18kag@gmail.com>
2019-10-27 14:41:37 +0900

    add birthday columnes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_052744) do
+ActiveRecord::Schema.define(version: 2019_10_27_053948) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2019_10_27_052744) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "birth_year"
+    t.integer "birth_month"
+    t.integer "birth_day"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
