d70f8b734d4e6f8e9b941546aaa93d13394c60c1
StByK <miz07og18kag@gmail.com>
2019-10-27 14:27:32 +0900

    modified sign_up view

--- db/migrate/20191027020710_devise_create_users.rb
+++ db/migrate/20191027020710_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :nickname,           null: false
-      t.string  :email,              null: false, unique: true, default: ""
-      t.string  :encrypted_password, null: false, default: ""
-      t.string  :birthday,           null: false
+      t.string  :nickname,            null: false
+      t.string  :email,               null: false, unique: true, default: ""
+      t.string  :encrypted_password,  null: false, default: ""
+      t.string  :birthday,            null: false
       t.string  :first_name,          null: false
       t.string  :last_name,           null: false
       t.string  :first_name_kana,     null: false
