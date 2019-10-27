d49628b93a1cfe5a27885e0eeb4030ed33f16c9b
StByK <miz07og18kag@gmail.com>
2019-10-27 14:35:53 +0900

    removed birthday column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_020710) do
+ActiveRecord::Schema.define(version: 2019_10_27_052744) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "birthday", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
