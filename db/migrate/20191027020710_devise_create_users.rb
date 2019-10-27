7dec2ac82d260a53625ab18acfc7a4142fc2b8a0
StByK <miz07og18kag@gmail.com>
2019-10-27 11:45:20 +0900

    updated migrate file (UserModel)

--- db/migrate/20191027020710_devise_create_users.rb
+++ db/migrate/20191027020710_devise_create_users.rb
@@ -4,8 +4,21 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :nickname,           null: false
+      t.string  :email,              null: false, unique: true, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :birthday,           null: false
+      t.string  :first_name,          null: false
+      t.string  :last_name,           null: false
+      t.string  :first_name_kana,     null: false
+      t.string  :last_name_kana,      null: false
+      t.integer :phone_number,        null: false
+      t.text    :profile_text,
+      t.string  :icon_image,
+      # 各モデル作った時に外部キーカラム作る！
+      # t.refferences :budgets_id,      null: false, foreign_key: true
+      # t.refferences :settlement_id,   null: false, foreign_key: true
+
 
       ## Recoverable
       t.string   :reset_password_token
