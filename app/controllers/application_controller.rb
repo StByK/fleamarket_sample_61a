af4349512e7ffdfd5f58ccfbb2c4be400f000e9a
StByK <miz07og18kag@gmail.com>
2019-10-27 15:06:41 +0900

    changed devise strong-parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [
-        :nickname, :birthday, :first_name, :last_name,
-        :first_name_kana, :last_name_kana, :phone_number
+        :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,
+        :phone_number, :birth_year, :birth_month, :birth_day
       ])
   end
 end
