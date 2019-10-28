9aaf1aedf6652f2175c263532648feebfe0bb676
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-28 22:33:44 +0900

    add gem 'haml'
    haml使えるようにするgem'haml'導入しました

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get "sign_out", :to => "users/sessions#destroy"
   end
+
+  get 'logout', to: 'users#logout'
+
 end
