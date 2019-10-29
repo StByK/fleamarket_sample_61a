d044627049b06f392494b55e43180b8451d3a637
StByK <miz07og18kag@gmail.com>
2019-10-29 19:50:03 +0900

    created ItemTable

--- db/migrate/20191029100801_create_items.rb
+++ db/migrate/20191029100801_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string,  :name,                      null: false
-      t.string,  :description,               null: false
-      t.string,  :condition,                 null: false
-      t.string,  :shipment_fee,              null: false
-      t.string,  :shipment_method,           null: false
-      t.string,  :shipment_date,             null: false
-      t.integer, :prefecture_index,          null: false
-      t.integer, :price,                     null: false
-      t.string,  :size,
+      t.string  :name,                      null: false
+      t.string  :description,               null: false
+      t.string  :condition,                 null: false
+      t.string  :shipment_fee,              null: false
+      t.string  :shipment_method,           null: false
+      t.string  :shipment_date,             null: false
+      t.integer :prefecture_index,          null: false
+      t.integer :price,                     null: false
+      t.string  :size
 
       t.timestamps
     end
