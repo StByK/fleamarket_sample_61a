fd3d3493abc0b6acffb5daba37fd5646d30ae78d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-30 14:35:24 +0900

    tryout deploy.rb
    キャッシュ削除コマンドいれてみたり・・・ブランチdevelopment指定したり・・・

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,35 +3,39 @@
 
 lock '3.11.2'
 
-# Capistranoのログの表示に利用する
 set :application, 'fleamarket_sample_61a'
-
-# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:StByK/fleamarket_sample_61a.git'
 
-set :default_stage, "development"
+#これまじでいけるよな？？
+set :branch, "development"
 
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/ex61.pem']  ※例：~/.ssh/key_pem.pem
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+end
+
+
+# webサーバー再起動時にキャッシュを削除
+after :restart, :clear_cache do
+  on roles(:web), in: :groups, limit: 3, wait: 10 do
+    # Here we can do anything such as:
+    within release_path do
+      execute :rm, '-rf', release_path.join('tmp/cache')
+    end
   end
 end
