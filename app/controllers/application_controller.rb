12f66cb9900c03bc50e96a99750524f8a53217f8
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-31 14:50:24 +0900

    create authenticate_basic method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,4 +10,13 @@ class ApplicationController < ActionController::Base
         :phone_number, :birth_year, :birth_month, :birth_day
       ])
   end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '61_mercari_a' && password == '61a'
+    end
+  end
+
 end
