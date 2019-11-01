5ae55549ad55cf9b214b1bd14286927b2eac2918
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-01 16:13:14 +0900

    find.oauth method in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable,omniauth_providers: => [:facebook] # 一旦矢印
-    has_one :sns_credential, dependent: :destroy
+         :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
+  
+  has_one :sns_credential, dependent: :destroy
 
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :birth_year, :birth_month, :birth_day, presence: true
 
-def self.find_oauth(auth)
-  
-end
 
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
 
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id)
+      end
+    end
+    return user  
+  end
 end
 
