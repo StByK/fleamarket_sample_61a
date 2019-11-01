9487c0adcde8cfe241d8349e9ad9be716c2d137b
hidetonishi <next1993.4@gmail.com>
2019-11-01 23:39:15 +0900

    commit javascript cdn

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.slider-list').slick({
+      // prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+      // nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      speed: 800
+  });
+
+//   $('.slick-dots li').on('mouseover', function() {
+//     $('.slider').slick('goTo', $(this).index());
+//   });
+});
