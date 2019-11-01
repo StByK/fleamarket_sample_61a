2afa7c91dabbe8c65cd9c23d3d978095951bb105
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2019-11-01 13:35:45 +0900

    create routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-
-
   def index
+  end
+
+  def profile_top
+  end
+
+  def profile_edit
   end
 
   def logout
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  devise_for :users, controllers: {registrations: "users/registrations"}
+  root 'users#index' #メルカリトップページ
+  get 'profile_top' => 'users#profile_top' #ユーザーマイページ
+  get 'profile_edit' => 'users#profile_edit' #ユーザープロフィール編集ページ
+
+  devise_for :users, controllers: {registrations: "users/registrations"} #ユーザー新規登録画面
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
+    get "sign_in", :to => "users/sessions#new" #ユーザーログイン画面
     get "sign_out", :to => "users/sessions#destroy"
   end
 
   resources :users, only: :index do
     collection do
-      get :logout
+      get :logout #ログアウト確認画面
     end
   end
 
