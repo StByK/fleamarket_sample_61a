87f0bc95ebcc8bca203a6766d491f4e0483447a9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-01 21:32:55 +0900

    put: :post on facebook path

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,36 +1,26 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+  def facebook
+    callback_for(:facebook)
+  end
 
+  def google_oauth2
+    callback_for(:google)
+  end
 
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def callback_for(provider)
+    auth = request.env["omniauth.auth"]
+    session[:uid]      = auth.uid
+    session[:provider] = auth.provider
+    
+    @user = User.find_oauth(request.env["omniauth.auth"])
+    if @user != nil
+      sign_in_and_redirect @user, event: :authentication 
+    else
+      redirect_to new1_1_registrations_path
+    end
+  end
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
-
-
-
+  def failure
+    redirect_to root_path and return
+  end
 end
