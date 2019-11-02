69311a85cea2963cbdefffc5961a406590cf60c2
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-02 18:41:47 +0900

    delete authhelper

--- spec/support/auth_helper.rb
+++
@@ -1,8 +0,0 @@
-module AuthHelper
-  def basic_login
-    @request.env ||= {}
-    user = 'arekore'
-    pw = 'passarekore'
-    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
-  end
-end
