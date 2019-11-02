1c3faaf8ecb09f21ef5909da42f08e03c55c5cf0
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-02 19:22:51 +0900

    define item's validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :condition, presence: true
+  validates :shipping_fee, presence: true
+  validates :shipping_method, presence: true
+  validates :shipping_date, presence: true
+  validates :prefecture_index, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :psize, presence: true
+
 end
