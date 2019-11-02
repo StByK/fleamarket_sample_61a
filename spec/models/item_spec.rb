535be3f73d39bc7c66db90551a381622c7d220ec
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-02 21:26:48 +0900

    test for validation

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+RSpec.describe Item do
+  describe '#create' do
+
+    it "is invalid without a name" do
+      item = build(:item, name: nil)
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without a description" do
+      item = build(:item, description: nil)
+      item.valid?
+      expect(item.errors[:description]).to include("can't be blank")
+    end
+
+    it "is invalid without a condition" do
+      item = build(:item, condition: nil)
+      item.valid?
+      expect(item.errors[:condition]).to include("can't be blank")
+    end
+
+    it "is invalid without shipment_fee" do
+      item = build(:item, shipment_fee: nil)
+      item.valid?
+      expect(item.errors[:shipment_fee]).to include("can't be blank")
+    end
+
+    it "is invalid without shipment_method" do
+      item = build(:item, shipment_method: nil)
+      item.valid?
+      expect(item.errors[:shipment_method]).to include("can't be blank")
+    end
+
+    it "is invalid without prefecture_index" do
+      item = build(:item, prefecture_index: nil)
+      item.valid?
+      expect(item.errors[:prefecture_index]).to include("can't be blank")
+    end
+
+    it "is invalid without price" do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("can't be blank")
+    end
+
+    it "is invalid without size" do
+      item = build(:item, size: nil)
+      item.valid?
+      expect(item.errors[:size]).to include("can't be blank")
+    end
+
+  #   it "上記のカラムが全て存在すれば登録できる" do
+  #     item = build(:item)
+  #     expect(item).to be_valid
+  #   end
+
+  #   it "nameが40文字なら登録できる" do
+  #     item = build(:item, name: "あいうえおアイウエオあいうえおアイウエオあいうえおアイウエオあいうえおアイウエオ")
+  #     expect(item).to be_valid
+  #   end
+
+  #   it "nameが41文字なら登録できない" do
+  #     item = build(:item, name: "！あいうえおアイウエオあいうえおアイウエオあいうえおアイウエオあいうえおアイウエオ")
+  #     item.valid?
+  #     expect(item.errors[:name]).to include("は40文字以内で入力してください")
+  #   end
+
+  #   it "descriptionが1000文字なら登録できる" do
+  #     item = build(:item, description: "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃい")
+  #     expect(item).to be_valid
+  #   end
+
+  #   it "descriptionが1001文字なら登録できない" do
+  #     item = build(:item, description: "！ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃい")
+  #     item.valid?
+  #     expect(item.errors[:description]).to include("は1000文字以内で入力してください")
+  #   end
+
+  #   it "priceが数字でなければ登録できない" do
+  #     item = build(:item, price: "a")
+  #     item.valid?
+  #     expect(item.errors[:price]).to include("は数値で入力してください")
+  #   end
+
+  #   it "priceが300なら登録できる" do
+  #     item = build(:item, price: 300)
+  #     expect(item).to be_valid
+  #   end
+
+  #   it "priceが299なら登録できない" do
+  #     item = build(:item, price: 299)
+  #     item.valid?
+  #     expect(item.errors[:price]).to include("は300以上の値にしてください")
+  #   end
+
+  #   it "priceが9999999なら登録できる" do
+  #     item = build(:item, price: 9999999)
+  #   expect(item).to be_valid
+  #   end
+
+  #   it "priceが10000000なら登録できない" do
+  #     item = build(:item, price: 10000000)
+  #     item.valid?
+  #     expect(item.errors[:price]).to include("は9999999以下の値にしてください")
+  #   end
+
+  end
+end
