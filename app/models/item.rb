9fa8ea2419a0b7cb41cd0b44083a79f0a2e2152c
StByK <miz07og18kag@gmail.com>
2019-11-04 17:51:00 +0900

    set associations (with Item)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
+  belongs_to :brand
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
