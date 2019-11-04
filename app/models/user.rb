a69926e10239d728eaa1adb01cb03ebaec5f132a
StByK <miz07og18kag@gmail.com>
2019-11-04 20:26:54 +0900

    set association with UserModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+  has_many :items, dependent: :destroy
   has_one :sns_credential, dependent: :destroy
 
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :birth_year, :birth_month, :birth_day, presence: true
