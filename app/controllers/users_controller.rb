3a35d77efe9a041e806cf0f20c409aa9edab3953
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-05 13:20:52 +0900

    solve conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,9 @@
 class UsersController < ApplicationController
+
   def index
-  end
-
-  def profile_top
-  end
-
-  def profile_edit
-  end
-
-  def product_details
   end
 
   def logout
   end
 
 end
-
