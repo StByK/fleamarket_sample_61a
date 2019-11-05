459ee6422dd339e7714849933cd2344def7460ee
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-05 22:39:46 +0900

    add show and edit controller instances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
+    @items = Item.where(user_id: @user.id)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
