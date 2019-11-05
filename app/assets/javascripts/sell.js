2eccabe9e49b24ad0a5330976f8a246522b63a03
arisan04 <arisan.0410.arisan@gmail.com>
2019-11-05 11:48:29 +0900

    ok

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,13 +1,49 @@
 
+$(function(){
+  $('#shipment-fee-select-tab').on('change',function(){
+    var selected = $('#shipment-fee-select-tab').val();
+    var add1 =`<div class="shipment-method">
+                <div class="select-delivery-add__shipment-method">
+                  <h2>配送の方法</h2>
+                  <span>必須</span>
+                </div>
+                <select class="select-tab" name="item[shipment_method]">
+                  <option value="">---</option>
+                  <option value="未定">未定</option>
+                  <option value="らくらくメルカリ便">らくらくメルカリ便</option>
+                  <option value="ゆうメール">ゆうメール</option>
+                  <option value="レターパック">レターパック</option>
+                  <option value="普通郵便(定形、定形外)">普通郵便(定形、定形外)</option>
+                  <option value="クロネコヤマト">クロネコヤマト</option>
+                  <option value="ゆうパック">ゆうパック</option>
+                  <option value="クリックポスト">クリックポスト</option>
+                  <option value="ゆうパケット">ゆうパケット</option>
+                </select>
+              </div>`
+    var add2 =`<div class="shipment-method">
+                <div class="select-delivery-add__shipment-method">
+                  <h2>配送の方法</h2>
+                  <span>必須</span>
+                </div>
+                <select class="select-tab" name="item[shipment_method]">
+                  <option value="">---</option>
+                  <option value="未定">未定</option>
+                  <option value="クロネコヤマト">クロネコヤマト</option>
+                  <option value="ゆうパック">ゆうパック</option>
+                  <option value="ゆうメール">ゆうメール</option>
+                </select>
+              </div>`
+    $('.shipment-method').empty()
 
-// $('.select-tab').change(function(){
-//   var add =`.select-delivery-add__shipment-method
-//               %h2 配送の方法
-//               %span 必須
-//             = f.select :shipment_method,["未定","らくらくメルカリ便"],{prompt: "---"},{class: "select-tab"}`
-//   $(".select-delivery-add-area").append(add)
-// })
-
+    if (selected == "") {
+      $('.shipment-method').empty()
+    } else if (selected == "送料込み(出品者負担)"){
+      $('.select-delivery-add-area').append(add1)
+    } else if (selected == "着払い(購入者負担)") {
+      $('.select-delivery-add-area').append(add2)
+    }
+  })
+})
 
 $(function(){
   $('.price-input').on('input', function(){
