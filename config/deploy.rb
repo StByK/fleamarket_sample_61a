a3212c6ba9e387e813c543c80fb1be52f8b158d8
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-06 15:58:53 +0900

    seed to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,22 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+
+namespace :deploy do
+  desc 'db_seed must be run only one time right after the first deploy'
+  task :db_seed do
+    on roles(:db) do |host|
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
+
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -28,12 +44,12 @@ namespace :deploy do
   end
 end
 
-namespace :deploy do
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  end
-end
+# namespace :deploy do
+#   desc "reload the database with seed data"
+#   task :seed do
+#     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+#   end
+# end
 
 
 # # webサーバー再起動時にキャッシュを削除
