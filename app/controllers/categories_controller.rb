0fa30102a39256ee5d652d9b70aeebbbea401dd3
StByK <miz07og18kag@gmail.com>
2019-11-07 21:01:53 +0900

    create index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,21 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
+
+  def index
+    @parents = Category.all.roots
+  end
 
   def show
-    @items = Item.all.where(category_id == params[:id])
+    @items = Item.all.where(category_id: @category.id)
+  end
+
+  private
+
+  def set_category
+    categories = Category.all.where(id: params[:id])
+    categories.each do |category|
+      @category = category.children
+    end
   end
 
 end
