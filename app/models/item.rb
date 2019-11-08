edecf76e0cad5061cac6a321693e2dd3dbf39875
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2019-11-08 20:18:55 +0900

    complited

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,12 @@ class Item < ApplicationRecord
   validates :prefecture_index, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :size, presence: true
+  
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+ 
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
 end
