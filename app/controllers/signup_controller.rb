d32e16ec97c16662f1d10d2f135dde3f26cc1ff1
coro-from-Kyoto <coro1527@gmail.com>
2019-11-10 13:49:59 +0900

    remove comment outs

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,18 +1,44 @@
 class SignupController < ApplicationController
+  # before_action :validates_step1, only: :step2 
+
 
   def new
     @user = User.new 
   end
   
-  #  各アクションごとに新規インスタンスを作成し、遷移元のページのデータをsessionに保管
   def step1
     @user = User.new 
   end
 
+  # def validates_step1
+  #   session[:nickname]              = user_params[:nickname]
+  #   session[:email]                 = user_params[:email]
+  #   session[:password]              = user_params[:password]
+  #   session[:password_confirmation] = user_params[:password_confirmation]
+  #   session[:first_name]            = user_params[:first_name]
+  #   session[:last_name]             = user_params[:last_name]
+  #   session[:first_name_kana]       = user_params[:first_name_kana]
+  #   session[:last_name_kana]        = user_params[:last_name_kana]
+  #   session[:birth_year]            = user_params[:birth_year]
+  #   session[:birth_month]           = user_params[:birth_month]
+  #   session[:birth_day]             = user_params[:birth_day]
+  #   @user = User.new(
+  #     nickname:                   session[:nickname],
+  #     email:                      session[:email],
+  #     password:                   session[:password],
+  #     password_confirmation:      session[:password_confirmation],
+  #     first_name:                 session[:first_name],
+  #     last_name:                  session[:last_name],
+  #     first_name_kana:            session[:first_name_kana],
+  #     last_name_kana:             session[:last_name_kana],
+  #     birth_year:                 session[:birth_year],
+  #     birth_month:                session[:birth_month],
+  #     birth_day:                  session[:birth_day])
+  #     render step1_signup_index_path unless @user.valid?
 
+  # end
 
   def step2
-    # step1で入力された値をsessionに保存
     session[:nickname]              = user_params[:nickname]
     session[:email]                 = user_params[:email]
     session[:password]              = user_params[:password]
@@ -29,14 +55,12 @@ class SignupController < ApplicationController
   end
 
   def step3
-    # step2で入力された値をsessionに保存
     session[:phone_number]          = user_params[:phone_number]
     @user = User.new
     @user.build_house
   end
 
   def step4
-    # step3で入力された値をsessionに保存
     # session[:first_name_house_attributes]           = user_params[:first_name]
     # session[:last_name_house_attributes]            = user_params[:last_name]
     # session[:first_name_kana_house_attributes]      = user_params[:first_name_kana]
@@ -55,7 +79,7 @@ class SignupController < ApplicationController
 
   def done
     # step4で入力された値をsessionに保存
-    # sign_in User.find(session[:user_id]) unless user_signed_in?
+    sign_in User.find(session[:user_id]) unless user_signed_in?
 
   end
 
