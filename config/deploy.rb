fcfef3f178424ec777d90a30ae2160ba83f6e420
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-14 01:41:57 +0900

    fix: delete secret.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,8 +29,6 @@ set :default_env, {
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
 
-set :linked_files, %w{ config/secrets.yml }
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -44,19 +42,6 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'upload secrets.yml'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
 
 
 # NOTE: seedファイル読み込みが必要な場合のみコメントアウトはずして使用
