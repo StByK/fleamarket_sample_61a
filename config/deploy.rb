33214d438e0130cc334704077fc8b543d3431fef
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-11-13 20:00:52 +0900

    add: db:reset task

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,20 +40,32 @@ namespace :deploy do
 end
 
 # NOTE: seedファイル読み込みが必要な場合のみコメントアウトはずして使用
-# namespace :deploy do
+namespace :deploy do
 
-#   desc 'reload the database with seed data'
-#   task :seed do
-#     on roles(:db) do
-#       with rails_env: fetch(:rails_env) do
-#         within release_path do
-#           execute :bundle, :exec, :rake, 'db:seed'
-#         end
-#       end
-#     end
-#   end
-#   after  :migrate,      :seed
-# end
+  desc 'reset the database'
+  task :db_reset do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:migrate:reset"
+        end
+      end
+    end
+  end
+
+
+  desc 'reload the database with seed data'
+  task :seed do
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within release_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  after  :migrate,      :seed
+end
 
 
 # rails runner db/category_seed.rb
